eec824c417043efff72f8dab88bf5e3f9de151df
HagarMohamedGamal <hagar.mohamed.gamal@gmail.com>
2020-03-27 13:33:58 +0200

    check no one hacks you and send an id of post creator that doesn’t exist in the database

---
+++ app/Http/Requests/StorePostRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|unique:posts|min:3',
+            'description' => 'required|min:10',
+            'user_id' => 'exists:App\User,id'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.min' => "you enterd less than 3 chars",
+            'description.min' => "you enterd less than 10 chars"
+        ];
+    }
+
+}
---
+++ app/Http/Requests/UpdatePostRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdatePostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        // dd(request()->post);
+        return [
+            'title' => [
+                'required',
+                Rule::unique('posts')->ignore(request()->post),
+                'min:3'
+            ],
+            'description' => 'required|min:10',
+            'user_id' => 'exists:App\User,id'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.min' => "you enterd less than 3 chars",
+            'description.min' => "you enterd less than 10 chars"
+        ];
+    }
+
+}
